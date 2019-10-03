46aa0e4bc17c866cc6e08f0a63e262e68f6c6103
lukec3x <lucasc3x@gmail.com>
2019-10-02 22:11:42 -0300

    Adicionando as informações da nuvem

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const amqp = require('amqplib/callback_api')
 const User = require('./models/user')
 
-amqp.connect('amqp://localhost:5672', function (err, conn) {
+// amqp.connect('amqp://localhost:5672', function (err, conn) {
+amqp.connect('amqp://xxypubma:Wn-dt1JagVPLaqaxRbz4Remvlf6acIeJ@llama.rmq.cloudamqp.com/xxypubma', function (err, conn) {
 
     conn.createChannel(function (err, ch) {
         var ex = 'pub_sub_meetup28'
@@ -12,11 +13,15 @@ amqp.connect('amqp://localhost:5672', function (err, conn) {
             console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", q.queue)
             ch.bindQueue(q.queue, ex, '')
 
-            ch.consume(q.queue, function async (msg) {
-                console.log(" [x] %s", msg.content.toString())
-                console.log(msg)
+            ch.consume(q.queue,  (msg) => {
+                // console.log(msg.content.toJSON())
+                // console.log(" [x] %s", msg.content.toString())
+                console.log(JSON.parse(msg.content)[0])
+                console.log(JSON.parse(msg.content))
+                // console.log(msg)
 
-                const user = await User.create(msg.content)
+                // const user = await User.create(msg.content)
+                // console.log(user)
 
             }, { noAck: true })
         })
--- pub.js
+++ pub.js
@@ -1,16 +1,15 @@
 const amqp = require('amqplib/callback_api')
 
-amqp.connect('amqp://localhost:5672', function (err, conn) {
+// amqp.connect('amqp://localhost:5672', function (err, conn) {
+amqp.connect('amqp://xxypubma:Wn-dt1JagVPLaqaxRbz4Remvlf6acIeJ@llama.rmq.cloudamqp.com/xxypubma', function (err, conn) {
 
     conn.createChannel(function (err, ch) {
         var ex = 'pub_sub_meetup28'
         // var msg = process.argv.slice(2).join(' ') || 'Hello World!'
-        var msg = {
-          name: 'lucas',
-        }
+        var msg = [{ id: 0000012123, name: 'Lucas',}, { id: 0000042123, name: 'Luke' }]
 
-        ch.assertExchange(ex, 'direct', { durable: false })
-        ch.publish(ex, '', new Buffer(msg))
+        ch.assertExchange(ex, 'fanout', { durable: false })
+        ch.publish(ex, '', Buffer.from(JSON.stringify(msg)))
         console.log(" [x] Sent %s", msg)
     })
 
